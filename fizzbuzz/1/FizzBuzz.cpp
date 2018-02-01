9abf1d79dbb5c4c47dcafe09a5ffb6c368f4d835
kn65op <kn65op@gmail.com>
2018-02-01 21:58:02 +0100

    Change to string

--- fizzbuzz/1/FizzBuzz.cpp
+++ fizzbuzz/1/FizzBuzz.cpp
@@ -7,39 +7,38 @@ using namespace testing;
 
 class Printer
 {
-public:
-  void print(std::vector<int> input, std::ostream& out)
-  {
-    if (!input.empty())
-        out << input.front() << "\n";
-  }
+  public:
+    void print(std::vector<std::string> input, std::ostream& out)
+    {
+        if (!input.empty())
+            out << input.front() << "\n";
+    }
 };
 
 struct PrinterTest : public Test
 {
     std::stringstream output;
     Printer printer;
-
 };
 
 TEST_F(PrinterTest, ShouldReturnEmptyTextForEmptyInput)
 {
-  std::vector<int> intput;
-  printer.print(intput, output);
+    std::vector<std::string> intput;
+    printer.print(intput, output);
 
-  ASSERT_THAT(output.str(), Eq(""));
+    ASSERT_THAT(output.str(), Eq(""));
 }
 
 TEST_F(PrinterTest, ShouldPrintNumberFollowedByNewline)
 {
-  std::vector<int> intput{1};
-  printer.print(intput, output);
+    std::vector<std::string> intput{"1"};
+    printer.print(intput, output);
 
-  ASSERT_THAT(output.str(), Eq("1\n"));
+    ASSERT_THAT(output.str(), Eq("1\n"));
 }
 
 int main(int argc, char** argv)
 {
-  InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
